c017964c865cd3f20a4ee608dcfeaeea673da54b
mspenski <44811974+mspenski@users.noreply.github.com>
2019-02-04 01:05:46 -0500

    got modal working and displaying dummy data name and picture, only thing left to do is create logic for most compatable match and then styling and touching up

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ var friendsArray = [
   {
     "name": "Rupert Holmes",
     "photo": "https://images-na.ssl-images-amazon.com/images/I/71cabFcIvUL._SL1048_.jpg",
-    "scores": [
+    "answers": [
       5,
       5,
       5,
@@ -13,6 +13,38 @@ var friendsArray = [
       5,
       5,
       5
+    ]
+  },
+  {
+    "name": "Not Rupert Holmes",
+    "photo": "https://images-na.ssl-images-amazon.com/images/I/71cabFcIvUL._SL1048_.jpg",
+    "answers": [
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3
+    ]
+  },
+  {
+    "name": "Definitely  not Rupert Holmes",
+    "photo": "https://images-na.ssl-images-amazon.com/images/I/71cabFcIvUL._SL1048_.jpg",
+    "answers": [
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1
     ]
   },
 ];
